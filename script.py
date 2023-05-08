eab455f803a9cdfc9ec12808e7618bab5f5e80a4
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-05-08 21:29:58 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 import scratchattach, os
 
-password = os.environ["SESSION"]
+session_id = os.environ["SESSION"]
 username = "StrangeIntensity"
 
-session = scratchattach.login(username, password)
+session = scratchattach.Session(session_id, username=username)
 
 project = session.connect_project(824262326)
 
-#project.post_comment(content="Nice!")
-print(session.session_id)
+project.post_comment(content="WOW")
+#print(session.session_id)
