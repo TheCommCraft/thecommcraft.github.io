758034fc70ad261f7879bf8b966b7f5b952e0bc5
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-05-10 21:08:18 +0200

    Update comments.py

--- comments.py
+++ comments.py
@@ -18,16 +18,16 @@ def load_comments():
   print("loading comments")
   project.update()
   users = {}
-  comments_root = get("https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/?limit=10").json()#project.comments(limit=10)
+  comments_root = get("https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/?limit=18").json()#project.comments(limit=10)
   comments = []
   for comment in comments_root:
     users[comment['author']['id']] = comment['author']['username']
     comments.append(f"{comment['author']['username']}: {comment['content']}")
-    comments.extend([(f"        {i['author']['username']}: @{users[i['commentee_id']]} {i['content']}", users.__setitem__(i['author']['id'], i['author']['username']))[0] for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=10").json()])
-    if len(comments) > 10:
+    comments.extend([(f"        {i['author']['username']}: @{users[i['commentee_id']]} {i['content']}", users.__setitem__(i['author']['id'], i['author']['username']))[0] for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=17").json()])
+    if len(comments) >= 18:
         break
   print(comments, comments_root)
-  return comments[:10]
+  return comments[:18]
 
 
 print("running server")
