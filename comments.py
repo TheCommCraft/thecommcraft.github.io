ae915ae86f9e448f4cae8d3846844b51639911fd
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-05-10 20:50:07 +0200

    Update comments.py

--- comments.py
+++ comments.py
@@ -17,11 +17,13 @@ client = scratchattach.CloudRequests(conn)
 def load_comments():
   print("loading comments")
   project.update()
+  users = {}
   comments_root = get("https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/?limit=10").json()#project.comments(limit=10)
   comments = []
   for comment in comments_root:
+    users[comment['author']['id']] = comment['author']['username]
     comments.append(f"{comment['author']['username']}: {comment['content']}")
-    comments.extend([f"        {i['author']['username']}: @{comment['author']['username']} {i['content']}" for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=10").json()])
+    comments.extend([(f"        {i['author']['username']}: @{users[i['commentee_id']]} {i['content']}", users.__setitem__(i['author']['id'], i['author']['username']))[0] for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=10").json()])
     if len(comments) > 10:
         break
   print(comments, comments_root)
