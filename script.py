fced72d7078a72a42079246ea055c4d0a6c3e503
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-05-16 16:05:06 +0200

    Update script.py

--- script.py
+++ script.py
@@ -17,7 +17,7 @@ t_comments.start()
 t_server = Thread(target=run_server)
 t_server.start()
 
-time.sleep(1800)
+time.sleep(30)
 print("Done")
 pgid = os.getpgid(os.getpid())
 os.killpg(pgid, signal.SIGINT)
