589f5c93eff486659dbd95583e79f1b7fbfe38d0
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-06-04 10:50:36 +0200

    Update bouncyballs.py

--- bouncyballs.py
+++ bouncyballs.py
@@ -106,7 +106,10 @@ def save_level(level_id, level_name, *level_content):
   if level.get("creator", username) != username:
     return "error"
   if level_name == "comments":
-    level_name = list(filter(lambda x: x["author"]["username"] == username, get_comments()))[0]["content"]
+    try:
+      level_name = list(filter(lambda x: x["author"]["username"] == username, get_comments()))[0]["content"]
+    except:
+      return "No comment"
   newvalues = {"content": level_content, "name": level_name, "creator": username}
   if level_name == "Nothing":
     newvalues.pop("name")
