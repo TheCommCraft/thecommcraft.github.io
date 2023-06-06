bece9df4ae099bdea9c2a0cb3175e6e70dba2989
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-06-06 15:21:55 +0200

    Update bouncyballs.py

--- bouncyballs.py
+++ bouncyballs.py
@@ -57,6 +57,7 @@ proj = session.connect_project(856420361)
 
 events = CloudEvents(856420361)
 
+"""
 _last_timestamp = 0
 
 def last_timestamp(self):
@@ -70,7 +71,8 @@ def set_last_timestamp(self, timestamp):
 
 CloudRequests.last_timestamp = property(last_timestamp)
 CloudRequests.last_timestamp = CloudRequests.last_timestamp.setter(set_last_timestamp)
-  
+"""
+
 client = CloudRequests(conn, used_cloud_vars=["1", "2", "3"])
 
 '''@events.event
