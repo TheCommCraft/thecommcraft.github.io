2d0af9f43d0b4a16cb53fe7ea019197448246ea2
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-07-29 21:20:26 +0200

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,7 +1,7 @@
 import scratchattach, os, time
 from threading import Thread
 from requests import get
-
+from html import unescape
 
 session_id = os.environ["SESSION"]
 username = "StrangeIntensity"
@@ -22,8 +22,8 @@ def load_comments():
   comments = []
   for comment in comments_root:
     users[comment['author']['id']] = comment['author']['username']
-    comments.append(f"{comment['author']['username']}: {comment['content']}")
-    comments.extend([(f"        {i['author']['username']}: @{users[i['commentee_id']]} {i['content']}", users.__setitem__(i['author']['id'], i['author']['username']))[0] for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=17").json()])
+    comments.append(f"{comment['author']['username']}: {unescape(comment['content'])}")
+    comments.extend([(f"        {i['author']['username']}: @{users[i['commentee_id']]} {unescape(i['content'])}", users.__setitem__(i['author']['id'], i['author']['username']))[0] for i in get(f"https://api.scratch.mit.edu/users/TheCommCraft/projects/847888429/comments/{comment['id']}/replies?limit=17").json()])
     if len(comments) >= 18:
         break
   print(comments, comments_root)
